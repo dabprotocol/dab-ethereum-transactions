025dd44dc6a9ff48f6b7baaebe807ed17712a267
DAVID ZHAO <david.zhao@wetdesign.com>
2018-01-28 13:12:44 -0800

    get current block information

--- getBlocks.js
+++ getBlocks.js
@@ -1,10 +1,32 @@
-let Web3 = require('web3');
-let MongoClient = require('mongodb').MongoClient;
+const Web3 = require('web3');
+const MongoClient = require('mongodb').MongoClient;
 
-let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-let url = 'mongodb://localhost:27017';
-let dbName = 'dab_ethereum';
+const config = require('./config/config');
+const web3 = new Web3(new Web3.providers.HttpProvider(config.ethereumRPC));
 
-let config = require('./config/config');
+MongoClient.connect(config.mongodbConnectionString, function(err, client) {
+    let db = client.db(config.dbName);
 
-console.log(config);
+    setInterval(() => {
+        web3.eth.isSyncing(function (error, sync) {
+            if (!error) {
+                if (sync === true) {
+                    web3.reset(true);
+                } else if (sync) {
+                    let currentBlockHeight = sync.currentBlock;
+                    db.collection('info').updateOne(
+                        {type: 'blockInfo'},
+                        {$set: {currentBlockHeight: currentBlockHeight}},
+                        {upsert: true}
+                    );
+
+                } else {
+                    //finished sync
+                    console.log('finished sync');
+                }
+            } else {
+                console.log(error);
+            }
+        });
+    }, 1000);
+});
